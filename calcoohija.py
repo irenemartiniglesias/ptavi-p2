a195111f0937a58f2cd8bdf4eb751f4bf420ed68
irenemartiniglesias <i.martini@alumnos.urjc.es>
2016-09-21 20:29:34 +0200

    programa final calcoohija

--- calcoohija.py
+++ calcoohija.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*
 
+#Vamos a crear una calculadora que herede de la Clase Calculadora
+
 import sys 
 
 class Calculadora():
@@ -8,4 +10,37 @@ class Calculadora():
 		return op1 + op2
 	def minus(self ,op1, op2):
 		return op1 - op2
+
+class CalculadoraHija(Calculadora):
+	def multiply(self, op1, op2):
+		return op1 * op2
+	def divide(self, op1, op2):
+		try:
+			return op1 / op2
+		except ZeroDivisionError:
+			sys.exit("Division by zero is not allowed")
+
+def resultado(operacion, op1, op2):
+	if (operacion == "suma"):
+		total = CalculadoraHija().plus(op1, op2)
+	elif (operacion == "resta"):
+		total = CalculadoraHija().minus(op1,op2)
+	elif (operacion == "multiplica"):
+		total = CalculadoraHija().multiply(op1,op2)
+	elif (operacion == "divide"):
+		total = CalculadoraHija().divide(op1,op2)
+	else:
+		sys.exit("Error: the operation is incorrect")
+	return total
+
+if __name__ == "__main__":
+
+	try:
+		op1 = float(sys.argv[1])
+		operacion = sys.argv[2]
+		op2 = float(sys.argv[3])
+	except ValueError:
+		sys.exit("Error: Non numerical parameters")
+	print(resultado(operacion,op1,op2))
+	
 
