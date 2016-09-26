f793ca056dee067804673ef50091d3ebbade7335
Irene Martin Iglesias <imartini@gamma13.aulas.gsyc.urjc.es>
2016-09-26 10:38:18 +0200

    Fichero final 3.0

--- calcplusplus.py
+++ calcplusplus.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*
 
-import sys
 import csv
 from calcplus import resultado_fichero
 
+
 if __name__=="__main__":
-	fichero = sys.argv[1]
-	fich = open(fichero)
-	resultado_fichero(fich)
+
+	with open('fichero') as mifichero:
+		lineas = csv.reader(mifichero)
+		for linea in lineas:
+			print(linea)
