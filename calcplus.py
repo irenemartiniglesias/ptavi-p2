295ed84e1e8dea39b016b054bc4362a110bdd39c
Irene Martin Iglesias <imartini@gamma13.aulas.gsyc.urjc.es>
2016-09-26 10:38:11 +0200

    Fichero final 3.0

--- calcplus.py
+++ calcplus.py
@@ -2,35 +2,9 @@
 # -*- coding: utf-8 -*
 
 import sys
+import calcoohija
 
-class Calculadora():
-	def plus(self, op1, op2):
-		return op1 + op2
-	def minus(self, op1, op2):
-		return op1 - op2
 
-class CalculadoraHija(Calculadora):
-	def multiply(self, op1, op2):
-		return op1 * op2
-	def divide(self, op1, op2):
-		try:
-			return op1 / op2
-		except ZeroDivisionError:
-			sys.exit("Division by zero is not allowed")
-
-def resultado(operacion, op1, op2):
-	if (operacion == "suma"):
-		total = CalculadoraHija().plus(op1, op2)
-	elif (operacion == "resta"):
-		total = CalculadoraHija().minus(op1,op2)
-	elif (operacion == "multiplica"):
-		total = CalculadoraHija().multiply(op1,op2)
-	elif (operacion == "divide"):
-		total = CalculadoraHija().divide(op1,op2)
-	else:
-		sys.exit("Error: the operation is incorrect")
-	return total
-	
 def resultado_operaciones(lineas):
 	for linea in lineas:
 		linea_operacion= linea.split(",")
@@ -38,7 +12,7 @@ def resultado_operaciones(lineas):
 		resultado_final = float(linea_operacion[1])
 		for i in range(len(linea_operacion)-2):
 			num_añade = float(linea_operacion[i+2])
-			resultado_final= resultado(operando, resultado_final, num_añade)	
+			resultado_final= calcoohija.resultado(operando, resultado_final, num_añade)	
 	print(resultado_final)
 	
 
@@ -46,6 +20,7 @@ def lineas_fichero(lines):
 	for line in lines:
 		lineas =line.split()
 		total = resultado_operaciones(lineas)
+
 
 def resultado_fichero(fich):
 	lines =fich.readlines()
