7e16b5258ef1d1384a69142fcc0805d0df200a16
Irene Martin Iglesias <imartini@gamma13.aulas.gsyc.urjc.es>
2016-09-26 10:37:52 +0200

    Fichero final 3.0

--- calcoo.py
+++ calcoo.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*
 
-import sys 
+import sys
+
 
 class Calculadora():
-	def plus (self, op1,op2):
-		return op1 + op2 
-	def minus (self, op1,op2):
-		return op1 - op2
+
+
+    def plus(self, op1, op2):
+        return op1 + op2 
+        
+    def minus(self, op1, op2):
+        return op1 - op2
+        
 
 def resultado(operacion, op1, op2):
-	if (operacion == "suma"):
-		total = Calculadora().plus(op1, op2)
-	elif (operacion == "resta"):
-		total = Calculadora().minus(op1,op2)
-	else:
-		sys.exit("Error: the operation is incorrect")
-	return total
+    if (operacion == "suma"):
+        total = Calculadora().plus(op1, op2)
+    elif (operacion == "resta"):
+        total = Calculadora().minus(op1,op2)
+    else:
+        sys.exit("Error: the operation is incorrect")
+    return total
+    
 
 if __name__ == "__main__":
 
-	try:
-		op1 = float(sys.argv[1])
-		operacion = sys.argv[2]
-		op2 = float(sys.argv[3])
-	except ValueError:
-		sys.exit("Error: Non numerical parameters")
-	print(resultado(operacion,op1,op2))
-
+    try:
+        op1 = float(sys.argv[1])
+        operacion = sys.argv[2]
+        op2 = float(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+    print(resultado(operacion,op1,op2))
